b093b4536804cf50a9dbcf76def29279b5ef323b
F4NT4SM4W <146638675+F4NT4SM4W@users.noreply.github.com>
2023-10-02 14:18:40 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,22 +21,22 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['555596490425'] //ur owner number
+global.ownernomer = "555596490425" //ur owner number2
+global.ownername = "👻F4nt4sm4" //ur owner name
+global.ytname = "YT: F4nt4sm4w" //ur yt chanel name
+global.socialm = "GitHub: F4nt4sm4w" //ur github or insta name
+global.location = "Brasil" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
+global.botname = "BuuH"
+global.ownernumber = '555596490425'
+global.ownername = '👻F4nt4sm4'
+global.ownerNumber = ["555596490425@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@F4nt4sm4w"
+global.websitex = "https://youtu.be/F4nt4sm4w"
+global.wagc = "https://chat.whatsapp.com/DjNiePUya2eAG30I81HvUx"
+global.themeemoji = '👻'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.packname = "Sticker By"
